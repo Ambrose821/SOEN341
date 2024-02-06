99c75b5e4749ee2dc72b354bf2e4331695de6142
Ambrose821 <mclaughlinambrose@gmail.com>
2024-02-05 21:40:14 -0500

    Initial Commit

---
+++ react-client/src/App.js
@@ -0,0 +1,40 @@
+import React,{Component} from "react";
+import logo from './logo.svg';
+import './App.css';
+
+class App extends Component{
+  constructor(props){
+    super(props);
+    this.state ={apiResponse: ""};
+  }
+
+  async callAPI(){
+    try{
+      fetch("http://localhost:9000/apiRoute")
+      .then(res => res.text())
+      .then(res => this.setState({apiResponse: res}))
+      .catch(err => console.error(err));}catch(err){
+      console.log(err);
+    }
+  }
+
+  componentDidMount(){
+    this.callAPI();
+  }
+
+  render(){
+    return(
+    <div className = "App">
+      <header className = "App-header">
+        <img src ={logo} className ="App-logo" alt ="logo"/>
+        <h1 className ="App-title">Welcome to React
+        </h1>
+      </header>
+      <p className ="App-intro">{this.state.apiResponse}</p>
+      </div>
+    );
+  }
+}
+
+
+export default App;
