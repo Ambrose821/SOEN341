87efbd407a2841429ac37038d5f575926aa894d0
Cheese5teak <128755119+Cheese5teak@users.noreply.github.com>
2024-03-03 16:30:22 -0500

    Modified css

--- react-client/src/Pages/Global Components/Sidebar.jsx
+++ react-client/src/Pages/Global Components/Sidebar.jsx
@@ -1,41 +1,51 @@
 import React from "react";
-import { Link } from 'react-router-dom';
-import {useAuth} from '../../apiServices/AuthContext'
+import { Link, useLocation } from 'react-router-dom'; // Import useLocation
+import { useAuth } from '../../apiServices/AuthContext'
 import HomeIcon from '@mui/icons-material/Home';
 import LoginIcon from '@mui/icons-material/Login';
 import LogoutIcon from '@mui/icons-material/Logout';
 import TimeToLeaveIcon from '@mui/icons-material/TimeToLeave';
 
 function Sidebar() {
-  const {currentUser,useFlag,logout,isLoggedIn} = useAuth();
+  const { currentUser, useFlag, logout, isLoggedIn } = useAuth();
+  const location = useLocation(); // Get current location
+  
   return (
     <div className="sidebar">
       <ul className="sidebarList">
         <li>
-          <HomeIcon/>
-          <Link to="/">Home</Link>
+          <Link to="/" className={`sidebarLink ${location.pathname === '/' ? 'activeLink' : ''}`}> {/* Add activeLink class if current path is '/' */}
+            <HomeIcon className="icon" />
+            <span className="sidebarTitle">Home</span>
+          </Link>
         </li>
-        
+
         {isLoggedIn && (
-          <li onClick={logout}>
-            <LogoutIcon/>
-            <Link to="/"> Logout</Link>
-          </li>
-        )}
-        {isLoggedIn &&(
           <li>
-            <TimeToLeaveIcon/>
-            <Link to="/reservations">My Reservations</Link>
+            <Link to="/reservations" className={`sidebarLink ${location.pathname === '/reservations' ? 'activeLink' : ''}`}> {/* Add activeLink class if current path is '/reservations' */}
+              <TimeToLeaveIcon className="icon" />
+              <span className="sidebarTitle">My Reservations</span>
+            </Link>
           </li>
         )}
-        {
-          !isLoggedIn && (
-            <li>
-              <LoginIcon/>
-              <Link to="/login">LogIn</Link>
-            </li>
-          )
-        }
+
+        {isLoggedIn && (
+          <li>
+            <Link to="/" className={`sidebarLink ${location.pathname === '/' ? 'activeLink' : ''}`} onClick={logout}> {/* Add activeLink class if current path is '/' */}
+              <LogoutIcon className="icon" />
+              <span className="sidebarTitle">Logout</span>
+            </Link>
+          </li>
+        )}
+        
+        {!isLoggedIn && (
+          <li>
+            <Link to="/login" className={`sidebarLink ${location.pathname === '/login' ? 'activeLink' : ''}`}> {/* Add activeLink class if current path is '/login' */}
+              <LoginIcon className="icon" />
+              <span className="sidebarTitle">LogIn</span>
+            </Link>
+          </li>
+        )}
         {/* Add more navigation links as needed */}
       </ul>
     </div>
