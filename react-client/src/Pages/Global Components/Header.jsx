2d6556fa5af0e9e5eacf9e55f91028e142ffb3c6
Vinisha Manek <Vinisha@exoshop.com>
2024-03-07 21:43:39 -0500

    Added profile button and sign up/login CSS

--- react-client/src/Pages/Global Components/Header.jsx
+++ react-client/src/Pages/Global Components/Header.jsx
@@ -1,14 +1,31 @@
-import React from 'react';
+import React from "react";
+import { Link, useLocation } from 'react-router-dom'; // Import useLocation
+import { useAuth } from '../../apiServices/AuthContext'
+import AccountCircleIcon from '@mui/icons-material/AccountCircle';
 
 function Header() {
   
+  const location = useLocation(); // Get current location
+
+  const {currentUser, currentUserFirstName, currentUserLastName, currentUserFlag, useFlag, logout, isLoggedIn} = useAuth();
+
 
   return (
     <div className="header">
-        <div className="HeadTitle">
+        {/* <div className="HeadTitle">
           Header
+        </div> */}
+        
+        <div className="profile">
+        {isLoggedIn && (
+            <Link to="/profile" className={`sidebarLink ${location.pathname === '/profile' ? 'activeLink' : ''}`}>{/* Add activeLink class if current path is '/profile' */}
+              <AccountCircleIcon className="icon" />
+              <span className="sidebarTitle">My Profile</span>
+            </Link>
+        )}
         </div>
     </div>
+    
   );
 }
 
