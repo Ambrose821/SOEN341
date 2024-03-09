7cf75e9838c26d073911da8b1413831a6c4dc9b9
christopher <christophermezzacappa818@yahoo.ca>
2024-03-09 17:42:04 -0500

    Reservation Adding and Seeing CRUD

--- api/routes/vehicles.js
+++ api/routes/vehicles.js
@@ -3,7 +3,9 @@ var router = express.Router();
 var Vehicle = require('../models/Vehicle');
 var mongo = require('mongodb');
 var assert = require('assert');
-var { addCar, deleteCar } = require('../controller/vehicleController')
+var { addCar, deleteCar } = require('../controller/vehicleController');
+const { isValidObjectId } = require('mongoose');
+var User = require('../models/User');
 
 router.get('/getCars', async function(req, res, next){
 
@@ -16,6 +18,82 @@ router.get('/getCars', async function(req, res, next){
         console.log(error);
         res.status(400).json({sucess:false,message: "Get vehicles Failed" + error})
     }
+});
+
+router.post('/reserve',async function(req,res,next){
+    try{
+
+        const {vehicleId, startDate,endDate, currentUser}= req.body;
+
+        const car = await Vehicle.findOne({_id: vehicleId}).lean();
+        const photoUrl = car.photoURL;
+        if(!car){
+            return res.status(404).json({message:"Vehicle Not Found"});
+        }
+
+        // creates new object of reservation 
+        const reservationToAdd = {
+            start:startDate,
+            end: endDate
+        };
+
+        //updates the the rez in the DB
+        await Vehicle.findOneAndUpdate(
+            {_id: vehicleId},
+            {$push : {reservation:reservationToAdd}},
+            {new: true}
+        );
+
+        const NewRez = {
+            start:startDate,
+            end:endDate,
+            photo:photoUrl
+        }
+
+        await User.findOneAndUpdate(
+            {email: currentUser},
+            {$push : {reservations: NewRez }},
+            {new: true}
+        );
+
+        res.status(200).json({message: 'Reservation sucessfull'});
+    }
+    catch(error){
+        console.log(error);
+        res.status(500).json({message: "An error occured"});
+    }
+});
+
+
+router.get('/getCarPhoto', async function(req, res, next){
+    const vehicleID = req.query.id;
+    
+    const car = await Vehicle.findOne({_id:vehicleID }).lean();
+
+    const PhotoUrl = car.photoURL;
+
+    res.status(200).json({message: "Found Photo",photoURL: PhotoUrl});
+});
+
+router.get('/getReservation',async function(req,res,next){
+    try{
+        const currentUser = req.query.currentUser;
+
+        const user = await User.findOne({email:currentUser});
+    
+        if(!user){
+            res.status(404).json({message : "User not found"})
+        }
+    
+        const reservationsFound = user.reservations;
+    
+        console.log(reservationsFound);
+        res.status(200).json({message: "Found reservations", reservations: reservationsFound});
+    }
+    catch(error){
+        res.status(500).json({message: "Something horrible happened"});
+    }
+    
 });
 
 // isAdmin
